40da62f036e2128810feeedbc7d13744e8ea9801
DangMinhThe-K58CA <thekoy.95@gmail.com>
2017-02-22 10:04:01 +0700

    re init models and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,16 +17,12 @@ class User extends Authenticatable
     //
     protected $table = 'users';
     //
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'password'];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 }
