cc3ef56ef134408e4f900e0991b42de5ffcbfec0
DangMinhThe-K58CA <thekoy.95@gmail.com>
2017-02-22 09:06:05 +0700

    Fixed morph migration

--- database/migrations/2017_02_22_080430_update_users_table.php
+++ database/migrations/2017_02_22_080430_update_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class UpdateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
+        //
+        Schema::table('users', function (Blueprint $table) {
             $table->unsignedInteger('role')->default(1);
             $table->unsignedInteger('status')->default(0);
-            $table->string('email')->unique();
-            $table->string('password');
             $table->text('description');
             $table->string('avatar')->default('default.jpg');
-            $table->rememberToken();
-            $table->timestamps();
             $table->softDeletes();
         });
     }
@@ -35,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
